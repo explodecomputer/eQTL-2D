1b54021d4c50ccaa919a1737ef620ddc8a872f71
Gibran Hemani 0452495387 <uqgheman@cluster.qbi.uq.edu.au>
2012-06-02 21:25:15 +1000

    eqtl2d

--- eQTL_2D/v4/collate_results.R
+++ eQTL_2D/v4/collate_results.R
@@ -1,6 +1,55 @@
 
 # results files are massive. this script will trim and collate them into RData files
 
+datstat <- function(info, xmat, resphen)
+{
+    cat(nrow(info), "rows to be analysed\n")
+    a <- array(0, nrow(info))
+    g <- array(0, nrow(info))
+    for(i in 1:nrow(info))
+    {
+	if(i %% (nrow(info) / 100) == 0) cat(i/(nrow(info)/100)," ")
+	l <- info[i,]
+	if(is.na(l$chr1)) {
+	    g[i] <- NA
+	    a[i] <- NA
+	    next
+	}
+	mod <- anova(lm(resphen[,l$probeid] ~ xmat[,l$pos1] + xmat[,l$pos2] + as.factor(xmat[,l$pos1]) : as.factor(xmat[,l$pos2])))
+	g[i] <- sum(mod$Sum[1:3]) / mod$Sum[4]
+	a[i] <- sum(mod$Sum[1:2]) / sum(mod$Sum[1:3])
+
+    }
+    return(data.frame(g,a))
+}
+
+datstat2 <- function(info, xmat, resphen)
+{
+    cat(nrow(info), "rows to be analysed\n")
+    a <- array(0, nrow(info))
+    g <- array(0, nrow(info))
+    for(i in 1:nrow(info))
+    {
+        if(i %% (nrow(info) / 100) == 0) cat(i/(nrow(info)/100)," ")
+        l <- info[i,]
+        if(is.na(l$chr1)) {
+            g[i] <- NA
+            a[i] <- NA
+            next
+        }
+
+        mod <- anova(lm(resphen[,l$probeid] ~ as.factor(xmat[,l$pos1]) * as.factor(xmat[,l$pos2])))
+	print(mod)
+
+	print(tapply(resphen[,l$probeid], list(xmat[,l$pos1], xmat[,l$pos2]), mean))
+	print(table(xmat[,l$pos1], xmat[,l$pos2]))
+
+    }
+}
+
+
+
+
 read.egu <- function(rootname, threshold)
 {
     if(!file.exists(paste(rootname, ".txt", sep="")))
@@ -11,7 +60,8 @@ read.egu <- function(rootname, threshold)
 	    read.egu(rootname, threshold, test)
 	}
 	cat("Missing: ", rootname)
-	return (NA)
+	return(data.frame(chr1=NA, chr2=NA, pos1=NA, pos2=NA,
+            snp1=NA, snp2=NA, pfull=NA, pint=NA, df1=NA, df2=NA, complete=complete))
     }
 
     complete <- system(paste("grep -q \"# 25 x 25 :\" ", rootname, ".txt", sep=""))
@@ -21,7 +71,7 @@ read.egu <- function(rootname, threshold)
     dat$pfull <- with(dat, -log10(pf(Fval, df1, df2, lower.tail=F)))
     dat$pint <- with(dat, -log10(pf(Fint, 4, df2, lower.tail=F)))
     dat <- subset(dat, pfull >= threshold | pint >= threshold)
-    dat <- subset(dat, (Chr1 != Chr2) | (abs(SNP1 - SNP2) > 500))
+    dat <- subset(dat, (Chr1 != Chr2) | (abs(SNP1 - SNP2) > 50))
 
     if(nrow(dat) == 0)
     {
@@ -36,7 +86,7 @@ read.egu <- function(rootname, threshold)
     a <- with(dat, paste(pos1, pos2))
     a <- duplicated(a)
     dat <- dat[!a, ]
-    cat(nrow(dat))
+   # cat(nrow(dat))
     return(dat)
 }
 
@@ -67,41 +117,44 @@ read.phen <- function(rootname)
 ######################
 
 rootres <- commandArgs(T)[1]
-roothsq <- commandArgs(T)[2]
-rootphe <- commandArgs(T)[3]
-first <- commandArgs(T)[4]
-last <- commandArgs(T)[5]
-threshold <- as.numeric(commandArgs(T)[6])
-eqtlobj <- commandArgs(T)[7]
-output <- commandArgs(T)[8]
+phenfile <- commandArgs(T)[2]
+genofile <- commandArgs(T)[3]
+roothsq <- commandArgs(T)[4]
+first <- commandArgs(T)[5]
+last <- commandArgs(T)[6]
+threshold <- as.numeric(commandArgs(T)[7])
+eqtlobj <- commandArgs(T)[8]
+output <- commandArgs(T)[9]
+
 
 # probe | probe number | hsq | complete | chr1 | chr2 | pos1 | pos2 | snp1 | snp2 | Pfull | Pint | df1 | df2 
 # if probe has no values then single row with NA 
 
-dat <- data.frame()
+load(phenfile)
+load(genofile)
 load(eqtlobj)
+
+dat <- data.frame()
 nid <- nrow(phendat)
-residdat <- phendat
 
 for(i in first:last)
 {
     cat(i, "... ")
     res <- read.egu(paste(rootres, i, sep=""), threshold)
     hsq <- read.hsq(paste(roothsq, i, sep=""))
-    phen <- read.phen(paste(rootphe, i, sep=""))
     
     probeid <- names(phendat)[i+2]
     res$probe <- probeid
     res$probeid <- i
     res$hsq <- hsq
-    
+    temp <- datstat(res, xmat, resphen)
+    res$propG <- temp$g
+    res$propA <- temp$a
+
     dat <- rbind(dat, res)
-    residdat[,i] <- phen
-    
     cat("\n")
 }
 
-residdat <- residdat[, first:last]
-save(residdat, dat, file=output)
+save(dat, file=output)
 
 
--- eQTL_2D/v4/submit_collate.sh
+++ eQTL_2D/v4/submit_collate.sh
@@ -3,15 +3,15 @@
 #$ -N collate
 #$ -cwd -V
 #$ -S /bin/bash
-#$ -t 1-100
+#$ -t 1-200
 
 #SGE_TASK_ID=${1}
 i=${SGE_TASK_ID}
-n=20
+n=10
 start=$(((i-1)*n+1))
 end=$((n*i))
 
-/clusterdata/apps/R-2.14/bin/R --no-save --args results/result scratch/resphen scratch/resphen ${start} ${end} 12 ../data/eqtl2d_objects.RData res${start}-${end}.RData < collate_results.R
+/clusterdata/apps/R-2.14/bin/R --no-save --args results/result ../data/residuals.RData ../data/clean_geno_final.RData scratch/resphen ${start} ${end} 12 ../data/eqtl2d_objects.RData res${start}-${end}.RData < collate_results.R
 
 
 
