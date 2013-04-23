6a7503aa9073a5ddc8bbe2ca9a1dc0b507378f35
Gibran Hemani 0452495387 <uqgheman@inode.(none)>
2013-04-24 03:37:45 +1000

    Performed filtering based on interaction p-values
    Do any 4.d.f. interaction pvalues surpass the threshold?
    - After filtering on the same criteria as bychr there are no
    signficant interactions

---
+++ filtering/submit_filter_interaction.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#$ -N epistasis
+#$ -cwd
+#$ -S /bin/bash
+#$ -t 1-1959
+#$ -o job_reports/
+#$ -e job_reports/
+
+if [ -n "${1}" ]; then
+  echo "${1}"
+  SGE_TASK_ID=${1}
+fi
+
+i=${SGE_TASK_ID}
+
+rootdir="/hpscratch/wrayvisscher/gib/git/eQTL-2D/"
+resdir="${rootdir}run/results/result"
+hsqdir="${rootdir}run/scratch/resphen"
+phendat="${rootdir}data/residuals.RData"
+genodat="${rootdir}data/geno.RData"
+threshold=15.38
+maxrsq=0.1
+output="${rootdir}v4_a/filtered_by_interaction/filtered_"
+minclass=5
+
+R --no-save --args ${i} ${resdir} ${hsqdir} ${phendat} ${genodat} ${threshold} ${maxrsq} ${minclass} ${output} < filter_raw_interaction.R
+
---
+++ filtering/submit_filter_interaction_2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#$ -N epistasis
+#$ -cwd
+#$ -S /bin/bash
+#$ -t 1-1959
+#$ -o job_reports/
+#$ -e job_reports/
+
+if [ -n "${1}" ]; then
+  echo "${1}"
+  SGE_TASK_ID=${1}
+fi
+
+i=${SGE_TASK_ID}
+
+rootdir="/hpscratch/wrayvisscher/gib/git/eQTL-2D/"
+resdir="${rootdir}run/results/result_2_"
+hsqdir="${rootdir}run/scratch/resphen_2_"
+phendat="${rootdir}data/residuals2.RData"
+genodat="${rootdir}data/geno.RData"
+threshold=15.38
+maxrsq=0.1
+output="${rootdir}v4_a/filtered_by_interaction/filtered_2_"
+minclass=5
+
+R --no-save --args ${i} ${resdir} ${hsqdir} ${phendat} ${genodat} ${threshold} ${maxrsq} ${minclass} ${output} < filter_raw_interaction.R
+
