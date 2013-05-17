8f546cf7307d8db0e212da8c468f131925454083
Gibran Hemani 0452495387 <uqgheman@cluster.qbi.uq.edu.au>
2013-05-17 10:21:29 +1000

    summarising replication

--- replication/results/summary_script.R
+++ replication/results/summary_script.R
@@ -4,11 +4,53 @@
 
 # Read in RData files
 
-load("/Volumes/group_wrayvisscher/josephP/Projects/Genomics/Expression_main/Epistasis/eQTL-2D/replication/results/EGCUT_replication.RData")
+load("~/repo/eQTL-2D/replication/results/EGCUT_replication.RData")
+newsig$id <- with(newsig, paste(probename, snp1, snp2))
 EGCUT <- newsig
-load("/Volumes/group_wrayvisscher/josephP/Projects/Genomics/Expression_main/Epistasis/eQTL-2D/replication/results/FehrmannHT12v3_replication.RData")
-Feh <- newsig
+
+load("~/repo/eQTL-2D/replication/results/FehrmannHT12v3_replication.RData")
+newsig$id <- with(newsig, paste(probename, snp1, snp2))
+Feh <- subset(newsig, select=-c(chr1, chr2, pos1, pos2, snp1, snp2, complete, probeid, probename, probechr, probegene, probehsq, minclasssize, snpcor, propG, propA, pnest, pfull, pint))
 rm(newsig)
+
+rep <- merge(EGCUT, Feh, by="id", suff=c("_EGCUT", "_Feh"), all=T)
+dim(rep)
+
+# Pairwise correlations between 
+cor(subset(rep, select=c(pnest, replication_pnest_EGCUT, replication_pnest_Feh)), use="pair")
+
+
+panel.cor <- function(x, y, digits=2, prefix="", cex.cor)
+{
+    usr <- par("usr"); on.exit(par(usr))
+    par(usr = c(0, 1, 0, 1))
+    r <- abs(cor(x, y, use="pair"))
+    txt <- format(c(r, 0.123456789), digits=digits)[1]
+    txt <- paste(prefix, txt, sep="")
+    if(missing(cex.cor)) cex <- 0.8/strwidth(txt)
+
+    test <- cor.test(x,y)
+    text(0.5, 0.5, txt, cex = cex)
+}
+
+
+
+temp <- subset(rep, select=c(pnest, replication_pnest_EGCUT, replication_pnest_Feh))
+names(temp) <- c("BSGS", "EGCUT", "Fehrmann")
+pdf("pnest_rep_pvals.pdf")
+pairs(temp, lower.panel=panel.smooth, upper.panel=panel.cor, main="Interaction p-values in 3 datasets (4df)")
+dev.off()
+
+temp <- subset(rep, select=c(pfull, replication_pfull_EGCUT, replication_pfull_Feh))
+names(temp) <- c("BSGS", "EGCUT", "Fehrmann")
+pdf("pfull_rep_pvals.pdf")
+pairs(temp, lower.panel=panel.smooth, upper.panel=panel.cor, main="Full genetic p-values in 3 datasets (8df)")
+dev.off()
+
+
+# QQ plots
+
+
 
 
 # Overlap in replication results
