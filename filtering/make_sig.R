63599995b938d3564ba905fe498c3f9d91ee4e9d
Gibran Hemani 0452495387 <uqgheman@cluster.qbi.uq.edu.au>
2013-05-19 00:22:17 +1000

    new filter list will include
    original 419
    new 30 that have no study wide significant marginal effects
    list of randomly chosen SNP pairs

---
+++ filtering/make_sig.R
@@ -0,0 +1,29 @@
+# make sig
+
+arguments <- commandArgs(T)
+aggfile <- arguments[1]
+T <- as.numeric(arguments[2])
+alpha <- as.numeric(arguments[3])
+outfile <- arguments[4]
+
+load(aggfile)
+
+sig <- subset(ld, pfull > T)
+sig <- subset(sig, pnest > -log10(alpha/nrow(sig)))
+
+SentinalSnp <- function(dat)
+{
+    dat$index <- 1:nrow(dat)
+    dat$code <- with(dat, paste(chr1, chr2, probename))
+    dat <- dat[order(dat$pnest), ]
+    dat <- subset(dat, !duplicated(code))
+    dat <- dat[order(dat$index), ]
+    dat <- subset(dat, select=-c(index, code))
+    return(dat)
+}
+
+sig <- SentinalSnp(sig)
+dim(sig)
+
+save(sig, file=outfile)
+
