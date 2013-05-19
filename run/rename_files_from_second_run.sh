8569cf8f346b1af8202b755320114b7a704b6141
Gibran Hemani 0452495387 <uqgheman@inode.(none)>
2013-05-19 16:15:38 +1000

    all 7339 results files have been re-archived and backed up as
    /fileserver/group/wrayvisscher/gib/repo/eQTL-2D/run/results/archive/eqtl2d_1.tar
    /fileserver/group/wrayvisscher/gib/repo/eQTL-2D/run/results/archive/eqtl2d_2.tar
    /fileserver/group/wrayvisscher/gib/repo/eQTL-2D/run/results/archive/eqtl2d_3.tar
    /fileserver/group/wrayvisscher/gib/repo/eQTL-2D/run/results/archive/eqtl2d_4.tar
    /fileserver/group/wrayvisscher/gib/repo/eQTL-2D/run/results/archive/eqtl2d_5.tar
    /fileserver/group/wrayvisscher/gib/repo/eQTL-2D/run/results/archive/eqtl2d_6.tar
    /fileserver/group/wrayvisscher/gib/repo/eQTL-2D/run/results/archive/eqtl2d_7.tar
    /fileserver/group/wrayvisscher/gib/repo/eQTL-2D/run/results/archive/eqtl2d_8.tar
    /fileserver/group/wrayvisscher/gib/repo/eQTL-2D/run/results/archive/eqtl2d_9.tar
    
    this is 34G of gzipped text files, named result*.txt.gz and with all result1*.txt.gz in eqtl2d_1.tar etc.

--- run/rename_files_from_second_run.sh
+++ run/rename_files_from_second_run.sh
@@ -4,10 +4,10 @@
 # There are 1959 like this
 # Need to add 5380 to all of them
 
-for i in {1..1959}
+for (( i=$1; i<=$2; i++ ))
 do
 
-  f="results/result_2_${i}.txt"
+  f="results/result_2_${i}.txt.gz"
 
   if [ ! -f ${f} ]; then
     echo "${f} does not exist!"
@@ -15,7 +15,7 @@ do
   fi
 
   j=$(($i+5380))
-  o="results/result${j}.txt"
+  o="results/result${j}.txt.gz"
 
   if [ -f ${o} ]; then
     echo "${o} already exists!"
