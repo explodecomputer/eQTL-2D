6f366550f6f9319689eefc4777318675f3cef5dd
Gibran Hemani <g.hemani@uq.edu.au>
2013-05-22 16:41:43 +1000

    added 3D plots to other probes that have multiple replicating interactions
    Some really interesting patterns emerge, particularly for TRAPPC5 and
    CAST

--- analysis/functional_analysis.R
+++ analysis/functional_analysis.R
@@ -6,7 +6,7 @@ library(reshape2)
 library(plyr)
 
 
-plot3D <- function(res, geno, phen, z=45)
+plot3d <- function(res, geno, phen, z=45)
 {
 	a <- tapply(phen[,res$probeid], list(geno[,res$pos1], geno[, res$pos2]), mean)
 	b <- table(geno[,res$pos1], geno[, res$pos2])
@@ -25,37 +25,61 @@ plot3D <- function(res, geno, phen, z=45)
 }
 
 
+plot3dProbe <- function(res, pg, geno, phen, z=45)
+{
+	a <- subset(res, probegene == pg)
+	l <- list()
+	for(i in 1:nrow(a))
+	{
+		l[[i]] <- plot3d(a[i,], xmat, resphen)[[3]]
+	}
+	do.call(grid.arrange, l)
+}
 
 
-
+#=================================================================================================#
+#=================================================================================================#
 
 
 load("~/repo/eQTL-2D/analysis/replication_summary.RData")
-
-a <- subset(sig, probegene == "TMEM149")
-
-l <- list()
-for(i in 1:nrow(a))
-{
-	l[[i]] <- plot3D(a[i,], xmat, resphen)[[3]]
-}
+load("~/repo/eQTL-2D/data/residuals_all.RData")
+load("~/repo/eQTL-2D/data/clean_geno_final.RData")
 
 
-b <- subset(sig, probegene == "MBNL1")
-
-l2 <- list()
-for(i in 1:nrow(b))
-{
-	l2[[i]] <- plot3D(a[i,], xmat, resphen)[[3]]
-}
-
-pdf("~/repo/eQTL-2D/analysis/TMEM149.pdf", width=15, height=15)
-do.call(grid.arrange, l)
+pdf("~/repo/eQTL-2D/analysis/images/TMEM149.pdf", width=15, height=15)
+plot3dProbe(sig, "TMEM149", xmat, resphen)
 dev.off()
 
-pdf("~/repo/eQTL-2D/analysis/MBNL1.pdf", width=15, height=15)
-do.call(grid.arrange, l2)
+pdf("~/repo/eQTL-2D/analysis/images/MBNL1.pdf", width=15, height=15)
+plot3dProbe(sig, "MBNL1", xmat, resphen)
 dev.off()
+
+pdf("~/repo/eQTL-2D/analysis/images/CAST.pdf", width=15, height=15)
+plot3dProbe(sig, "CAST", xmat, resphen)
+dev.off()
+
+pdf("~/repo/eQTL-2D/analysis/images/TRAPPC5.pdf", width=15, height=15)
+plot3dProbe(sig, "TRAPPC5", xmat, resphen)
+dev.off()
+
+pdf("~/repo/eQTL-2D/analysis/images/HMBOX1.pdf", width=15, height=15)
+plot3dProbe(sig, "HMBOX1", xmat, resphen)
+dev.off()
+
+pdf("~/repo/eQTL-2D/analysis/images/NAPRT1.pdf", width=15, height=15)
+plot3dProbe(sig, "NAPRT1", xmat, resphen)
+dev.off()
+
+
+#=================================================================================================#
+#=================================================================================================#
+
+
+
+
+
+
+
 
 
 
