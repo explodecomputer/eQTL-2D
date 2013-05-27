36a604469727d8935de9ca7e7c803dd736609e83
Gibran Hemani <g.hemani@uq.edu.au>
2013-05-27 11:10:34 +1000

    condensed replication results into repliaction2_summarised.RData

---
+++ replication/results/summary_script2.R
@@ -0,0 +1,29 @@
+library(lattice)
+library(latticeExtra)
+library(gridExtra)
+
+
+summariseRep <- function(filename)
+{
+	load(filename)
+	newsig$gcm <- gcm
+	newsig$gcs <- gcs
+
+	v <- matrix(0, nrow(newsig), 8)
+	for(i in 1:nrow(newsig))
+	{
+		v[i,] <- mod[[i]]$variances[-1]
+	}
+	v <- as.data.frame(v)
+	nom <- names(mod[[1]]$variances[-1])
+	names(v) <- nom
+
+	sig <- cbind(newsig, v)
+
+	return(sig)
+}
+
+fehr <- summariseRep("~/repo/eQTL-2D/replication/results/replication_GrngHT12v3.RData")
+egcut <- summariseRep("~/repo/eQTL-2D/replication/results/replication_EGCUT.RData")
+
+save(fehr, egcut, file="~/repo/eQTL-2D/replication/results/replication2_summarised.RData")
