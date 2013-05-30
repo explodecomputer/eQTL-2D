294b4a97a76c962de5a77e96c65a5828b86d2fab
Gibran Hemani <tghemani@trident1.hpc.int.tri.edu.au>
2013-05-30 23:26:57 +1000

    50kb sim finished

--- analysis/chromosome_interactions/simulation_pbs.sh
+++ analysis/chromosome_interactions/simulation_pbs.sh
@@ -3,7 +3,7 @@
 #PBS -N ci2
 #PBS -o /home/tghemani/repo/ibd_ibs/grm/scripts/job_reports/
 #PBS -e /home/tghemani/repo/ibd_ibs/grm/scripts/job_reports/
-#PBS -J 1001-10000
+#PBS -J 1-10000
 
 
 set -e
@@ -18,7 +18,7 @@ i=${PBS_ARRAY_INDEX}
 cifile="~/repo/eQTL-2D/data/supFile3_K562_interactingLoci_clusters.csv"
 bimfile="~/repo/eQTL-2D/data/clean_geno_final.bim"
 n=549
-win=25000
+win=500000
 outroot="~/repo/eQTL-2D/analysis/chromosome_interactions/results/out"
 
 R --no-save --args ${i} ${n} ${win} ${cifile} ${bimfile} ${outroot} < ~/repo/eQTL-2D/analysis/chromosome_interactions/simulation.R
