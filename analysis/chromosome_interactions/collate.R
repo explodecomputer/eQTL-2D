0fd8317de93d195297ec9c98f3b18b575333e88c
Gibran Hemani <tghemani@trident1.hpc.int.tri.edu.au>
2013-05-30 22:55:00 +1000

    chromosome interactions - 10000 2Mb permutations finished

--- analysis/chromosome_interactions/collate.R
+++ analysis/chromosome_interactions/collate.R
@@ -1,7 +1,7 @@
-a <- array(NA, 1000)
+a <- array(NA, 10000)
 win <- "1e+06"
 n <- 549
-for(i in 1:1000)
+for(i in 1:10000)
 {
 	a[i] <- scan(paste("results/out", n, win, i, sep="_"), what=numeric())
 }
