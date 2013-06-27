fad43631aae6151fa6e0c2abb196384285fedd03
Gibran Hemani <explodecomputer@gmail.com>
2013-06-27 16:37:34 +1000

    updated interaction lists to have replication variance components

--- replication/results/summary_script2.R
+++ replication/results/summary_script2.R
@@ -8,6 +8,8 @@ summariseRep <- function(filename)
 	for(i in 1:nrow(newsig))
 	{
 		v[i,] <- mod[[i]]$variances[-1]
+		vp <- var(mod[[i]]$phen, na.rm=TRUE)
+		v[i,] <- v[i,] / vp
 	}
 	v <- as.data.frame(v)
 	nom <- names(mod[[1]]$variances[-1])
@@ -22,3 +24,10 @@ fehr <- summariseRep("~/repo/eQTL-2D/replication/results/replication_GrngHT12v3.
 egcut <- summariseRep("~/repo/eQTL-2D/replication/results/replication_EGCUT.RData")
 
 save(fehr, egcut, file="~/repo/eQTL-2D/replication/results/replication2_summarised.RData")
+
+
+
+load("~/repo/eQTL-2D/replication/results/replication_GrngHT12v3.RData")
+
+
+
