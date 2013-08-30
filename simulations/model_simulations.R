c8e324a1a8e5d312a8ddcc7f8dcbfadb176b44c1
joseph powell <joseph.powell@uq.edu.au>
2013-08-30 16:21:18 +1000

    added code to calculate type 1 and 2 and produce figure

--- simulations/model_simulations.R
+++ simulations/model_simulations.R
@@ -66,22 +66,17 @@ for(i in 1:(length(pval)-29)) {
 }
 
 
-
-
 # NCP of chi sq
 # Peter's suggestion
 
-
-
 sim2.fun <- function(
-	h2, 					# heritability of cis effect
 	thres.8df, 
-	n,
-	sim
-	
+	sim,
+	NCP
 	){
 
-	NCP <- n*(h2/(1-h2))
+
+	#NCP <- n*(h2/(1-h2))
 	chi8 <- array(0, sim)
 	chi4 <- array(0, sim)
 	p8 <- array(0, sim)
@@ -102,48 +97,35 @@ sim2.fun <- function(
 
 	out <- as.data.frame(cbind(chi8, chi4, p8, p4))
 	names(out) <- c("chi8", "chi4", "p8", "p4")
-#	return(out)
 
 	thres.index <- which(out$p8 > thres.8df)
-	type1 <- length(which(out$p4[thres.index] > -log10(0.05))) / length(thres.index)
-	list(above.thres=length(thres.index), type1=type1, ncp=NCP, fullresults=out)
+	l <- length(thres.index)	
+	type1 <- length(which(out$p4[thres.index] > 1.3)) / l
+	type2 <- (l/sim)
+	list(type1=type1, type2=type2, ncp=NCP, fullresults=out)
 }
 
 
 
-tmp <- sim2.fun(0.09, 16, 846, 1000000)
-tmp$ncp
-tmp$type1
+tmp <- sim2.fun(16, 100000, 85)
 
-qchisq(10^-16, 8, lower.tail=F)
+#tmp$type2
+#tmp$type1
+
+ty1 <- c()
+ty2 <- c()
+for(i in seq(50, 150, 5)) {
+	tmp <- sim2.fun(16, 100000, i)
+	ty1[i] <- tmp$type1
+	ty2[i] <- tmp$type2
+}
+
+
+plot(ty2,ty1,xlab="power (1st stage)", ylab="type 1 error rate 2ns stage")
 
 
 
-#################
-# Peter's email.
-#################
 
-Dear both,
-
-Re(1). I have thought about this a bit more and think that this can be approximated by simulation but without having to simulate individual trait values (i.e. can be done easily and fast). The 8df threshold is about 91. If we assume that the null model is that there is one additive cis-effect only (reasonable for a null model I think), then both the null and alternative (4 df test) can be done by simulating chisquare statistics (from normal deviates):
-
-Null mode: chi(8) = sum(z^2), with z ~N(0,1) variates for 7 value, and for 1 value z ~ N(mu,1), with mu = sqrt(NCP). 
-
-NCP is the chisquare non-centrality parameter pertaining to the cis-effect. If the cis-effects explains h^2 of phenotypic variance for the trait, then NCP = N * h^2 / (1 - h^2), with N the sample size (~ 500 in our case). I'm not sure what a reasonable value of NCP is for the study (maybe 50?) but we can get this from the previous BSGS publications. 
-
-Reduced model: chi(4) = chi(8) - sum[z^2], where chi(8) is the already simulated value and the sum is over the simulated z-statistics from 5 to 8, including the z-statistics pertaining to the cis-effect.
-
-So a simple simulation algorithm would be:
-1. Set 8df threshold and set NCP
-2. Simulate 8 z-statistics and perform test
-3. If chi(8) > threshold then perform 4 df test
-4. Count if 4df test statistic is above a 0.05 threshold for a 4df chisquare distribution (= 9.49)
-
-Please let me know if you think that this would work and if it is worthwhile doing. I'm curious about the actual type-I error rate among  the 500 selected pairs.
-
-Cheers,
-
-Peter
 
 
 
