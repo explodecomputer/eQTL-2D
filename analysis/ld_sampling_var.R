d78c3e768ebc7f8b9ade83f95dd4e3eb0666e2fa
Gibran Hemani <explodecomputer@gmail.com>
2013-09-06 17:18:06 +1000

    work continues on rebuttal

--- analysis/ld_sampling_var.R
+++ analysis/ld_sampling_var.R
@@ -29,6 +29,7 @@ sampleSnp <- function(r, n, p1, q1)
 	x22 <- r * sqrt(p1*q1*p2*q2) + p2*q2
 
 	xs <- c(x11, x12, x21, x22)
+
 	if(any(xs < 0))
 	{
 		return(NA)
@@ -62,10 +63,10 @@ doSim <- function(param)
 # Create parameters
 param <- expand.grid(
 	r = c(0, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99), 
-	n = c(900, 1200, 20000), 
-	p1 = c(0.25, 0.5, 0.75), 
-	q1 = c(0.25, 0.5, 0.75), 
-	sim = 1:100, r_obs = NA
+	n = c(50, 900, 1200, 20000), 
+	p1 = c(0.1, 0.2, 0.3, 0.4, 0.5), 
+	q1 = c(0.1, 0.2, 0.3, 0.4, 0.5), 
+	sim = 1:50, r_obs = NA
 )
 param <- subset(param, p1 >= q1)
 
@@ -77,12 +78,12 @@ param <- doSim(param)
 
 
 # Summarise results
-p <- subset(param, !is.na(r_obs), select=c(r, r_obs, n))
+p <- subset(param, !is.na(r_obs), select=c(r, r_obs, n, p1, q1))
 ps <- rbind(p, p, p, p, p)
 ps$pow <- rep(c(1,2,4,6,8), each=nrow(p))
 ps$r_obs <- ps$r_obs^ps$pow
 
-ps_summary <- ddply(ps, .(pow, r, n), summarise, 
+ps_summary <- ddply(subset(ps), .(pow, r, n), summarise, 
 	N    = length(r_obs),
 	sd   = sd(r_obs),
 	se   = sd(r_obs) / sqrt(length(r_obs)),
@@ -91,21 +92,39 @@ ps_summary <- ddply(ps, .(pow, r, n), summarise,
 	qu3  = quantile(r_obs, prob=0.75, na.rm=T)	
 )
 
+subset(ps_summary, r==0.5 & pow==2)
 
 #=================================================================#
 
 # Plot
-ggplot(ps_summary, aes(x = pow, y = sd)) +
+ggplot(subset(ps_summary, n > 100), aes(x = pow, y = sd)) +
 geom_point() +
 geom_line(aes(colour=factor(n))) +
 facet_grid(. ~ r) + 
-labs(x = "Power term", y = "Standard deviation", colour = "Sample size") 
+labs(x = "Power term", y = "Standard deviation", colour = "Sample size") +
+scale_colour_brewer(type="qual")
 ggsave(file="~/repo/eQTL-2D/analysis/images/ld_sampling_sd.pdf", width=10, height=6)
 
-ggplot(ps_summary, aes(x = pow, y = mean)) +
+ggplot(subset(ps_summary, n > 100), aes(x = pow, y = mean)) +
 geom_point() +
 geom_line(aes(colour=factor(n))) +
 facet_grid(. ~ r) + 
-labs(x = "Power term", y = "Mean", colour = "Sample size") 
+labs(x = "Power term", y = "Mean", colour = "Sample size") +
+scale_colour_brewer(type="qual")
 ggsave(file="~/repo/eQTL-2D/analysis/images/ld_sampling_mean.pdf", width=10, height=6)
 
+ggplot(subset(ps_summary, r != 0), aes(x = pow, y = sd^2 / mean)) +
+geom_point() +
+geom_line(aes(colour=factor(n))) +
+facet_grid(. ~ r) + 
+labs(x = "Power term", y = "Coefficient of variance", colour = "Sample size") +
+scale_colour_brewer() +
+theme_bw()
+
+ggplot(subset(ps, r != 0 & n > 100), aes(y = r_obs, x = factor(pow))) +
+geom_boxplot(aes(fill = factor(n))) +
+facet_grid( . ~ r)
+
+ggplot(subset(ps, r != 0 & n > 100), aes(y = r_obs, x = factor(pow))) +
+geom_point(aes(colour = factor(n)), position="dodge") +
+facet_grid( . ~ r)
