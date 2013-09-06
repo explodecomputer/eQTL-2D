ee39014d8c25b15882ff7e0ccad49e24b9dc9d20
Konstantin Shakhbazov <Konstantin.Shakhbazov@gmail.com>
2013-09-06 13:52:49 +1000

    typos

--- func_annot/lib/get_genome_annotations.sh
+++ func_annot/lib/get_genome_annotations.sh
@@ -2,6 +2,7 @@
 # http://ftp.ebi.ac.uk/pub/databases/ensembl/encode/integration_data_jan2011/byDataType/segmentations/jan2011/hub/genomeSegmentation.html
 
 # get me K562 combined chromatin states
+cd ../DATA
 wget http://ftp.ebi.ac.uk/pub/databases/ensembl/encode/integration_data_jan2011/byDataType/segmentations/jan2011/hub/k562.combined.bb
 
 # get me Gencode16 genome annotation
