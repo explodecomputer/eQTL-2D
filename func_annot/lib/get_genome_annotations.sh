a52697378777480dd875a426d385d3b04e0dc047
Konstantin Shakhbazov <Konstantin.Shakhbazov@gmail.com>
2013-09-06 13:42:48 +1000

    download genome annotation and conver to sane format as if anything in bioinformatics had a sane format

---
+++ func_annot/lib/get_genome_annotations.sh
@@ -0,0 +1,8 @@
+# Here is overview of genome-segmentation:
+# http://ftp.ebi.ac.uk/pub/databases/ensembl/encode/integration_data_jan2011/byDataType/segmentations/jan2011/hub/genomeSegmentation.html
+
+# get me K562 combined chromatin states
+wget http://ftp.ebi.ac.uk/pub/databases/ensembl/encode/integration_data_jan2011/byDataType/segmentations/jan2011/hub/k562.combined.bb
+
+# get me Gencode16 genome annotation
+wget ftp://ftp.sanger.ac.uk/pub/gencode/release_16/gencode.v16.annotation.gtf.gz
