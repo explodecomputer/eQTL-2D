5ec784e1ca2b0a40c9a0c354946680544c411d12
Joseph Powell <joseph.powell@uq.edu.au>
2014-03-12 12:30:25 +1000

    edits committed and westra files

--- impute_fine_map/fine_mapping.R
+++ impute_fine_map/fine_mapping.R
@@ -21,7 +21,9 @@
 #load("/ibscratch/wrayvisscher/josephP/BSGS/Imputed/Epistasis/agg_filtered.RData")
 
 info <- read.csv("/fileserver/group/wrayvisscher/josephP/Projects/Genomics/Expression_main/Epistasis/eQTL-2D/Frayling_et_al/data_files/inc_info_bsgs.csv", header=T)
-load("/ibscratch/wrayvisscher/josephP/BSGS/Imputed/Epistasis/residuals.RData")
+#load("/ibscratch/wrayvisscher/josephP/BSGS/Imputed/Epistasis/residuals.RData")
+load("/fileserver/group/wrayvisscher/josephP/Projects/Genomics/Expression_main/Epistasis/eQTL-2D/data/bsgs_egcut_fehr_data.RData")
+bsgs <- phenlist[[1]]
 
 
 #=======================================================#
@@ -35,35 +37,43 @@ make_snp_region_files.fun(info)
 #		RUN THE EPISCAN ON THE IMPUTED DATA 			#
 #=======================================================#
 
-
-for
-
-
 blockdir <- "/ibscratch/wrayvisscher/josephP/BSGS/Imputed/Epistasis/snp_region_data/"
+outdir <- "/fileserver/group/wrayvisscher/josephP/Projects/Genomics/Expression_main/Epistasis/eQTL-2D//impute_fine_map/frayling_results/"
 
-ped1 <- read.table(paste(blockdir, as.character(info$SNP1[i]), ".ped", sep=""), header=F)
-ped2 <- read.table(paste(blockdir, as.character(info$SNP2[i]), ".ped", sep=""), header=F)
- 
-map1 <- read.table(paste(blockdir, as.character(info$SNP1[i]), ".map", sep=""), header=F)
-map2 <- read.table(paste(blockdir, as.character(info$SNP2[i]), ".map", sep=""), header=F)
+for(i in 1:nrow(info)) {
+	ped1 <- read.table(paste(blockdir, as.character(info$SNP1[i]), ".ped", sep=""), header=F)
+	ped2 <- read.table(paste(blockdir, as.character(info$SNP2[i]), ".ped", sep=""), header=F)
+	 
+	map1 <- read.table(paste(blockdir, as.character(info$SNP1[i]), ".map", sep=""), header=F)
+	map2 <- read.table(paste(blockdir, as.character(info$SNP2[i]), ".map", sep=""), header=F)
 
-block1 <- plink_to_012.fun(ped1, map1)
-block2 <- plink_to_012.fun(ped2, map2)
+	block1 <- plink_to_012.fun(ped1, map1)
+	block2 <- plink_to_012.fun(ped2, map2)
 
-pheno <- resphen[,which(colnames(resphen)==as.character(info$Probe[i]))]
+	pheno <- bsgs[,which(colnames(bsgs)==as.character(info$Probe[i]))]
 
-epi_scan_out <- epi_scan.fun(block1, block2, pheno)
-
-
-
-epi_scan_out[which.max(epi_scan_out$intP),]
-
-#
+	epi_scan_out <- epi_scan.fun(block1, block2, pheno)
+	write.csv(epi_scan_out, paste(outdir, as.character(info$Probe[i]), "_", as.character(info$SNP1[i]), "_", as.character(info$SNP2[i]), ".csv", sep=""), quote=F, row.names=F)
+}
 
 
-snp_info <- read.table("/ibscratch/wrayvisscher/josephP/BSGS/Imputed/Var_eQTL/bsgs_imputed_R2_80_cleaned_stage2_chr_all_SNP_info.txt", header=T)
-snp_info_geno <- read.table("/ibscratch/wrayvisscher/josephP/BSGS/Genotype/GWAS.map", header=T)
-# pheno
-load("/ibscratch/wrayvisscher/josephP/BSGS/Imputed/Epistasis/residuals.RData")
+
+
+
+#=======================================================#
+#		ANALYSIS OF THE IMPUTED REGION EPI SCAN 		#
+#=======================================================#
+
+tmp <- read.csv(paste(outdir, as.character(info$Probe[i]), "_", as.character(info$SNP1[i]), "_", as.character(info$SNP2[i]), ".csv", sep=""), header=T)
+filter <- which(tmp$rsq < 0.2 & tmp$nclass==9 & tmp$minclass > 5)
+tmp2 <- tmp[filter,]
+tmp2[which.max(tmp2$intP),]
+
+
+
+
+tmp[which.max(tmp$intP),]
+
+lf <- list.files()
 
 
