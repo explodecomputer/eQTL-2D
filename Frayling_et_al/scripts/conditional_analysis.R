9b6e9a5490df1b879e60f9dacf4f4cd4b02d80d9
Joseph Powell <joseph.powell@uq.edu.au>
2014-03-13 16:27:17 +1000

    conditional analysis added

--- Frayling_et_al/scripts/conditional_analysis.R
+++ Frayling_et_al/scripts/conditional_analysis.R
@@ -28,51 +28,19 @@ ped <- read.table("/fileserver/group/wrayvisscher/josephP/Projects/Genomics/Expr
 map <- read.table("/fileserver/group/wrayvisscher/josephP/Projects/Genomics/Expression_main/Epistasis/eQTL-2D/Frayling_et_al/test/snp_list.map", header=F)
 block <- plink_to_012.fun(ped, map)
 
-
-
-# file mapped by Kostya
 westra <- 
 
-# read in 
 
 
 
 
 
-three_snp_test.fun <- function(info, block, bsgs){
-
-	out <- array(0, c(nrow(info), 4)) 
-
-	for(i in 1:nrow(info)) {
-		
-		if(i == 6) {
-
-			out[i,] <- NA
-		}
-
-		else {
-			snp1  <- block[,which(colnames(block)==as.character(info$SNP1[i]))] 
-			snp2  <- block[,which(colnames(block)==as.character(info$SNP2[i]))] 
-			inc_snp <- block[,which(colnames(block)==as.character(info$rs_id[i]))] 
-
-			pheno <- bsgs[,which(colnames(bsgs)==as.character(info$Probe[i]))]
-			pheno_adj <- summary(lm(pheno ~ inc_snp))$residuals
 
 
-			fullmod <- lm(pheno ~ as.factor(inc_snp) + as.factor(snp1) + as.factor(snp2) + as.factor(snp1):as.factor(snp2):as.factor(inc_snp))
-			redmod <- lm(pheno ~ as.factor(snp1) + as.factor(snp2) + as.factor(inc_snp))
-			# This is the interaction terms on their own (nested test)
-			intmod <- anova(redmod, fullmod)	
-			# Extract statistics	
-			tmp <- summary(fullmod)$fstatistic
-			out[i,1] <- round(-log10(pf(tmp[1], tmp[2], tmp[3], low=F)),2)		
-			out[i,2] <- (summary(fullmod))$fstatistic[2]	
-			out[i,3] <- round(-log10(intmod$Pr[2]), 2)
-			out[i,4] <- 
-		}
-	}
 
-	colnames(out) <- c("")
+three_snp_analysis <- three_snp_test.fun(info, block, bsgs)
+
+
 
 
 
