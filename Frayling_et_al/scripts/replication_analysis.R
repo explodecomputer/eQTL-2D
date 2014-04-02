6c23df01062e82e225c0777e3acae445eb0b0218
Joseph Powell <joseph.powell@uq.edu.au>
2014-04-02 18:41:09 +1000

    updates

---
+++ Frayling_et_al/scripts/replication_analysis.R
@@ -0,0 +1,15 @@
+# analysis of the egcut and ferhman replication datasets
+
+# Read in data (one at a time)
+
+# egcut
+load("/Users/jpowell/repo/eQTL-2D/Frayling_et_al/replication_analysis/results/egcut/egcut-logtransformedquantilenormalized/replication_plus.RData")
+
+#load("/Users/jpowell/repo/eQTL-2D/Frayling_et_al/replication_analysis/results/egcut/egcut-40pcs/replication_plus.RData")
+
+
+# ferhmann
+load("/Users/jpowell/repo/eQTL-2D/Frayling_et_al/replication_analysis/results/Groningen/groningen-logtransformedquantilenormalized/replication_plus.RData")
+
+#load("/Users/jpowell/repo/eQTL-2D/Frayling_et_al/replication_analysis/results/Groningen/groningen-40pc/replication_plus.RData")
+
