955f8d9d472e520798be6ba6835a91cd3d6588ae
Joseph Powell <joseph.powell@uq.edu.au>
2014-04-09 17:12:18 +1000

    final version for others added

--- Frayling_et_al/scripts/replication_analysis.R
+++ Frayling_et_al/scripts/replication_analysis.R
@@ -4,21 +4,38 @@
 
 # egcut
 load("/Users/jpowell/repo/eQTL-2D/Frayling_et_al/replication_analysis/results/egcut/egcut-logtransformedquantilenormalized/replication_plus.RData")
-
+egcut1 <- fit
 load("/Users/jpowell/repo/eQTL-2D/Frayling_et_al/replication_analysis/results/egcut/egcut-40pcs/replication_plus.RData")
-
+egcut2 <- fit
 
 # ferhmann
 load("/Users/jpowell/repo/eQTL-2D/Frayling_et_al/replication_analysis/results/Groningen/groningen-logtransformedquantilenormalized/replication_plus.RData")
+feh1 <- fit
 
 load("/Users/jpowell/repo/eQTL-2D/Frayling_et_al/replication_analysis/results/Groningen/groningen-40pcs/replication_plus.RData")
+reh2 <- fit
+
+
+
+combine <- function(x,y) return(pchisq(-2*log(x/2)-2*log(y/2),4,low=F))
+i <- 26
+
+egcut1[[i]]$snp1
+egcut1[[i]]$snp2
+egcut1[[i]]$probe
+egcut1[[i]]$incsnp
+
+egcut1[[i]]$replication_pnest
+egcut2[[i]]$replication_pnest
+feh1[[i]]$replication_pnest
+feh2[[i]]$replication_pnest
+
+
+
+out <- combine(a,b)
+combine(out,c)
 
 
 
 
 
-for(i in 1:26) {
-
-print(fit[[i]]$replication_pnest)
-
-}
