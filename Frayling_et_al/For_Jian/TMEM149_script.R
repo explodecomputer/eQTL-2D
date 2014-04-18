f9c5dab049237661ec7f468eed7097effab34123
Joseph Powell <joseph.powell@uq.edu.au>
2014-04-18 14:26:27 +1000

    datafile and analysis script for Jian

---
+++ Frayling_et_al/For_Jian/TMEM149_script.R
@@ -0,0 +1,16 @@
+
+# Analayis of regression model function
+# Joseph Powell
+# 18/04/2014
+
+
+# load TMEM149 datafile
+
+# Object names TMEM149, contains snp genotypes and probe levels for 846 individual
+load("/Users/jpowell/repo/eQTL-2D/Frayling_et_al/For_Jian/TMEM149.RData")
+
+
+
+
+
+
