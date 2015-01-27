ec771896ee646c146dea761e1fd82b2007ffb69d
Joseph Powell <joseph.powell@uq.edu.au>
2015-01-27 16:52:46 +1000

    investigation script

--- investigation/scripts/epi_empirical.R
+++ investigation/scripts/epi_empirical.R
@@ -7,3 +7,10 @@
 # sig data
 load('~/repo/eQTL-2D/investigation/data/sig_501_data.RData')	
 
+# Genotype data
+load('~/repo/eQTL-2D/data/geno.RData')
+
+# Probe (residuals) data
+load('~/repo/eQTL-2D/data/bsgs_egcut_fehr_data.RData')
+bsgs <- phenlist[[1]]
+rm(phenlist)
