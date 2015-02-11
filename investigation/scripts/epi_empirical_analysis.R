9f2f3fc9a155d83ec9a57202c29b281afefdf01d
Joseph Powell <joseph.powell@uq.edu.au>
2015-02-12 08:15:02 +1000

    new function script and edits

--- investigation/scripts/epi_empirical_analysis.R
+++ investigation/scripts/epi_empirical_analysis.R
@@ -6,63 +6,46 @@
 # read in the functions
 source("/Users/jpowell/repo/eQTL-2D/investigation/scripts/epi_empirical_analysis_fun.R")
 
+# load data
+load("/Users/jpowell/repo/eQTL-2D/investigation/data/investigation_data.RData")
 
-
-##################################################################
-##################################################################
-##################################################################
-
-# Determine the summary of the output
 # set the data dir
 setwd("/Users/jpowell/repo/eQTL-2D/investigation/data/output/")
 
+# list files
 lf <- list.files()
+
+
+##################################################################
+##################################################################
+##################################################################
+# summary of the clean and total snps
+
+genome_summary <- genome_sum.fun(lf)
+
+
+##################################################################
+##################################################################
+##################################################################
+# add the filter info and y/n in 30 sig
+
+genome_summary <- filter_add.fun(sig, genome_summary)
+
+
+
+##################################################################
+##################################################################
+##################################################################
+# Determine the summary of the output
 
 lambda <- summarize.fun(lf)
 
 
 
-
+##################################################################
+##################################################################
+##################################################################
 # Calculate the additive eQTL effects for each pair
-
-
-load("/Users/jpowell/repo/eQTL-2D/investigation/data/")
-
-add_cal.fun <- function(sig, bsgs, geno, bim) {
-
-	out <- matrix(0, nrow=nrow(sig), ncol=6)
-	for(i in 1:nrow(sig)) {
-
-		probe <- as.character(sig$probename[i])
-		snp1 <- as.character(sig$snp1[i])
-		snp2 <- as.character(sig$snp2[i])
-
-		pheno <- bsgs[,which(names(bsgs)==probe)]
-		geno1 <- geno[,which(names(geno)==snp1)]
-		geno2 <- geno[,which(names(geno)==snp2)]
-
-		# check everything is the correct length
-		if(length(geno1)!=846 | length(geno2)!=846 | length(pheno)!=846) {
-			stop(print(paste(probe, " incorrect data length")))
-		}
-
-		# Run single marker additive model
-		out[i,4] <- summary(lm(pheno~geno1))$coefficients[2,4]
-		out[i,5] <- summary(lm(pheno~geno2))$coefficients[2,4]
-		out[i,6] <- length(which(out[i,4:5] < 0.0000001))
-		# add names
-		out[i,1] <- probe
-		out[i,2] <- snp1	
-		out[i,3] <- snp2	
-
-	}
-
-	out <- as.data.frame(out)
-	names(out) <- c("probe", "snp1", "snp2", "addpval1", "addpval2", "nadd")
-	return(out)
-
-
-}
 
 add_test <- add_cal.fun(sig, bsgs, geno, bim)
 
--- investigation/scripts/epi_empirical_analysis_fun.R
+++ investigation/scripts/epi_empirical_analysis_fun.R
@@ -38,3 +38,113 @@ summarize.fun <- function(lf) {
 	names(out) <- c("probename", "snp1", "snp2", "nsnps", "lambda", "nthreshold")	
 	return(out)
 }
+
+
+
+
+
+# Calculate the additive eQTL effects for each pair
+add_cal.fun <- function(sig, bsgs, geno, bim) {
+
+	out <- matrix(0, nrow=nrow(sig), ncol=6)
+	for(i in 1:nrow(sig)) {
+
+		probe <- as.character(sig$probename[i])
+		snp1 <- as.character(sig$snp1[i])
+		snp2 <- as.character(sig$snp2[i])
+
+		pheno <- bsgs[,which(names(bsgs)==probe)]
+		geno1 <- geno[,which(names(geno)==snp1)]
+		geno2 <- geno[,which(names(geno)==snp2)]
+
+		# check everything is the correct length
+		if(length(geno1)!=846 | length(geno2)!=846 | length(pheno)!=846) {
+			stop(print(paste(probe, " incorrect data length")))
+		}
+
+		# Run single marker additive model
+		out[i,4] <- summary(lm(pheno~geno1))$coefficients[2,4]
+		out[i,5] <- summary(lm(pheno~geno2))$coefficients[2,4]
+		out[i,6] <- length(which(out[i,4:5] < 0.0000001))
+		# add names
+		out[i,1] <- probe
+		out[i,2] <- snp1	
+		out[i,3] <- snp2	
+
+	}
+
+	out <- as.data.frame(out)
+	names(out) <- c("probe", "snp1", "snp2", "addpval1", "addpval2", "nadd")
+	return(out)
+
+
+}
+
+
+
+# genome information summary
+genome_sum.fun <- function(lf) {
+	out <- matrix(0, nrow=length(lf), ncol=8)
+
+	for(i in 1:length(lf)) {
+		load(lf[i])
+
+		out[i,1] <- substr(lf[i], 1, 12)
+		out[i,2] <- strsplit(lf[i], "_")[[1]][3]
+		out[i,3] <- strsplit(lf[i], "_")[[1]][4]	
+
+		out[i,4] <- length(which(output$nclass == 9))
+		out[i,5] <- length(which(output$minclass > 5))
+		out[i,6] <- length(which(output$LD < 0.1))
+
+
+		out[i,7] <- length(which(output$nclass==9 & output$minclass > 5 & output$LD < 0.1))
+		out[i,8] <- nrow(output)
+
+		rm(output)
+
+		print(i)
+	}
+
+	out <- as.data.frame(out)
+	names(out) <- c("probename", "snp1", "snp2", "nclass9", "minclass5", "LD01", "nsnpspass", "nsnps")	
+	return(out)
+
+}
+
+
+
+# add information of the filter used and if it passed replication 
+filter_add.fun <- function(sig, gs) {
+
+
+	out <- matrix(0, nrow=nrow(gs), ncol=3)
+	for(i in 1:nrow(gs)) {
+
+		index <- which(sig$probename==gs$probename[i] & sig$snp1==gs$snp1[i] & sig$snp2==gs$snp2[i])
+		if(length(index)!=1) {
+
+			stop(print(i, "Error in the identification of matched probe and snps"))
+
+		}
+
+		out[i,1] <- sig$filter[index]
+		out[i,2] <- sig$pnest_egcut[index]
+		out[i,3] <- sig$pnest_fehr[index]
+
+	}
+
+	out <- as.data.frame(out)
+	names(out) <- c("filter", "pnest_egcut", "pnest_fehr")
+	out <- cbind(gs, out)
+	return(out)
+
+}
+
+
+
+
+
+
+
+
