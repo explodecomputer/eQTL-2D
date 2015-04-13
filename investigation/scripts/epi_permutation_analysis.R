440a26ad518620b5160e2f98637b937b9cc138e8
Joseph Powell <joseph.powell@uq.edu.au>
2015-04-14 09:10:49 +1000

    permutation analysis script

--- investigation/scripts/epi_permutation_analysis.R
+++ investigation/scripts/epi_permutation_analysis.R
@@ -14,9 +14,9 @@ analysis.fun <- function(dir, sig) {
 
 	setwd(dir)
 	lf <- list.files(dir)
-	out <- array(0, c(length(lf), 6))
+	out <- array(0, c(length(lf), 8))
 
-	for(i in 1:length(lf)) {
+	for(i in 1:10){#length(lf)) {
 		load(lf[i])
 
 		probe <- substr(lf[i], 1, 12)
@@ -30,7 +30,7 @@ analysis.fun <- function(dir, sig) {
 		foo$P <- -log10(foo$P)
 
 		pair <- which(sig$probename==probe & sig$snp1==snp1 & sig$snp2==snp2)
-		pemp <- which.min(abs(foo$P-sig$pnest[pair]))/10000000
+		pemp <- which.min(abs(foo$P-sig$pnest[pair]))/nrow(foo)
 
 		out[i,1] <- sig$probename[pair]
 		out[i,2] <- sig$probegene[pair]
@@ -38,18 +38,22 @@ analysis.fun <- function(dir, sig) {
 		out[i,4] <- sig$snp2[pair]
 		out[i,5] <- round(sig$pnest[pair], 3)
 		out[i,6] <- round(-log10(pemp), 3)
+		out[i,7] <- nrow(foo)
+		out[i,8] <- pemp
 
 		print(i)
 	}
 
 	out <- as.data.frame(out)
-	names(out) <- c("probename", "probegene", "snp1", "snp2", "pnest", "pemp")
+	names(out) <- c("probename", "probegene", "snp1", "snp2", "pnest", "neglog10pemp", "n", "pemp")
 	return(out)
 }
 
 
 pemp_out <- analysis.fun("~/repo/eQTL-2D/investigation/data/output_permutation/", sig)
 write.table(pemp_out, "~/repo/eQTL-2D/investigation/data/pemp_out.txt", quote=F, row.names=F)
+pemp_out <- read.table("~/repo/eQTL-2D/investigation/data/pemp_out.txt", header=T)
+
 
 
 png(filename="~/repo/eQTL-2D/investigation/docs/figures/pemp.png", width=600, height=600)
@@ -57,4 +61,12 @@ hist(as.numeric(as.matrix(pemp_out$pemp)), breaks=25,
 	xlab="permutation p - value",
 	main="501 Pairs 10,000,000")
 dev.off()
+
+
+
+
+
+
+
+
 
