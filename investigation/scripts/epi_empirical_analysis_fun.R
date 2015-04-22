ed7c5d39e27c3d803619ee5cf0cab48bbe9622d0
Joseph <joseph.powell@uq.edu.au>
2015-04-22 17:49:56 +1000

    epi script updated

--- investigation/scripts/epi_empirical_analysis_fun.R
+++ investigation/scripts/epi_empirical_analysis_fun.R
@@ -4,6 +4,49 @@
 # joseph.powell@uq.edu.au
 
 
+
+
+
+##################################################################
+##################################################################
+##################################################################
+# Calculate the additive eQTL effects for each pair
+add_cal.fun <- function(sig, bsgs, geno, bim) {
+
+	out <- matrix(0, nrow=nrow(sig), ncol=6)
+	for(i in 1:nrow(sig)) {
+
+		probe <- as.character(sig$probename[i])
+		snp1 <- as.character(sig$snp1[i])
+		snp2 <- as.character(sig$snp2[i])
+
+		pheno <- bsgs[,which(names(bsgs)==probe)]
+		geno1 <- geno[,which(names(geno)==snp1)]
+		geno2 <- geno[,which(names(geno)==snp2)]
+
+		# check everything is the correct length
+		if(length(geno1)!=846 | length(geno2)!=846 | length(pheno)!=846) {
+			stop(print(paste(probe, " incorrect data length")))
+		}
+
+		# Run single marker additive model
+		out[i,4] <- summary(lm(pheno~geno1))$coefficients[2,4]
+		out[i,5] <- summary(lm(pheno~geno2))$coefficients[2,4]
+		out[i,6] <- length(which(out[i,4:5] < 0.0000001))
+		# add names
+		out[i,1] <- probe
+		out[i,2] <- snp1	
+		out[i,3] <- snp2	
+
+	}
+
+	out <- as.data.frame(out)
+	names(out) <- c("probe", "snp1", "snp2", "addpval1", "addpval2", "nadd")
+	return(out)
+
+
+}
+
 ##################################################################
 ##################################################################
 ##################################################################
@@ -78,48 +121,6 @@ summarize.fun <- function(lf) {
 	return(out)
 }
 
-
-
-
-##################################################################
-##################################################################
-##################################################################
-# Calculate the additive eQTL effects for each pair
-add_cal.fun <- function(sig, bsgs, geno, bim) {
-
-	out <- matrix(0, nrow=nrow(sig), ncol=6)
-	for(i in 1:nrow(sig)) {
-
-		probe <- as.character(sig$probename[i])
-		snp1 <- as.character(sig$snp1[i])
-		snp2 <- as.character(sig$snp2[i])
-
-		pheno <- bsgs[,which(names(bsgs)==probe)]
-		geno1 <- geno[,which(names(geno)==snp1)]
-		geno2 <- geno[,which(names(geno)==snp2)]
-
-		# check everything is the correct length
-		if(length(geno1)!=846 | length(geno2)!=846 | length(pheno)!=846) {
-			stop(print(paste(probe, " incorrect data length")))
-		}
-
-		# Run single marker additive model
-		out[i,4] <- summary(lm(pheno~geno1))$coefficients[2,4]
-		out[i,5] <- summary(lm(pheno~geno2))$coefficients[2,4]
-		out[i,6] <- length(which(out[i,4:5] < 0.0000001))
-		# add names
-		out[i,1] <- probe
-		out[i,2] <- snp1	
-		out[i,3] <- snp2	
-
-	}
-
-	out <- as.data.frame(out)
-	names(out) <- c("probe", "snp1", "snp2", "addpval1", "addpval2", "nadd")
-	return(out)
-
-
-}
 
 
 ##################################################################
@@ -244,12 +245,105 @@ type1_30.fun <- function(gs, sig30) {
 
 
 
+#=======================================================#
+#	RUN THE ANALYSIS FOR STEP ONE OF THE INVESTIGATION 	#
+#=======================================================#
+
+
+analysis.fun <- function(probe, snp1, snp2, bsgs, geno, bim) {
+
+
+	pheno <- bsgs[,which(names(bsgs)==probe)]
+	geno1 <- geno[,which(names(geno)==snp1)]
+	geno2 <- geno[,which(names(geno)==snp2)]
+
+	# check everything is the correct length
+	if(length(geno1)!=846 | length(geno2)!=846 | length(pheno)!=846) {
+		stop(print(paste(i, " incorrect data length")))
+	}
+
+	# Run single marker additive model
+	add1 <- summary(lm(pheno~geno1))$coefficients[2,4]
+	add2 <- summary(lm(pheno~geno2))$coefficients[2,4]
+
+	# perform genome-wide anaysis
+	p <- which.min(c(add1, add2))
+	if(p==1) {
+		snp_fix <- snp1
+		snp_other <- snp2
+	}
+	if(p==2) {
+		snp_fix <- snp2
+		snp_other <- snp1
+	}
+
+	# make the 'remaining' geno
+	i1 <- which(bim$V1==bim$V1[which(bim$V2==snp_other)])
+	MB_plus5 <- bim$V4[which(bim$V2==snp_fix)]+5000000 
+	MB_minus5 <- bim$V4[which(bim$V2==snp_fix)]-5000000 
+	chr <- bim$V1[which(bim$V2==snp_fix)]
+	i2 <- which(bim$V1==chr & bim$V4 > MB_minus5 & bim$V4 < MB_plus5)
+	index <- unique(c(i1,i2))
+	
+	# none conflicted geno matrix
+	g <- geno[,-index]
+
+
+	out <- matrix(0, nrow=ncol(g), ncol=4)
+	telliter <- 1000
+	for(k in 1:nrow(out)) {
+
+		fit <- summary(lm(pheno~g[,k]))
+		out[k,1] <- fit$coefficients[2,4]
+		out[k,2] <- fit$df[2]
+		out[k,3] <- fit$coefficients[2,2]
+		out[k,4] <- fit$coefficients[2,3]
+
+		if(k %% telliter==0){
+			print(k)
+		}
+
+	}
+
+	out <- as.data.frame(out)
+	names(out) <- c("pval", "df", "se", "fstat")
+	return(out)
+
+}
 
 
 
+#=======================================================#
+#		CONVERT THE PED FORMT TO A 0, 1, 2, FORMAT 		#
+#=======================================================#
 
+plink_to_012.fun <- function(
+	ped, 		# ped file
+	map) {		# map file
 
+	ids <- ped[, 1:6]
+	nid <- nrow(ids)
+	ped <- ped[, -c(1:6)]
+	index <- seq(1, ncol(ped), 2)
+	geno <- matrix(0, nid, length(index))
 
+	# Convert to 0, 1, 2 format
+	for(i in 1:length(index)) {
+		snp <- ped[,c(index[i], index[i]+1)]
+		x <- array(NA, nid)
+		snp[snp == "0"] <- NA
 
+		i0 <- snp[,1] == 1 & snp[,2] == 1
+		i2 <- snp[,1] == 2 & snp[,2] == 2
+		i1 <- (snp[,1] == 1 & snp[,2] == 2) | (snp[,1] == 2 & snp[,2] == 1)
+		x[i0] <- 0
+		x[i1] <- 1
+		x[i2] <- 2
+		geno[, i] <- x
+	}
 
+	colnames(geno) <- map$V2
+	rownames(geno) <- ids$V2
+	return(geno)
+}
 
