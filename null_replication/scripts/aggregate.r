9d9aeaafd32a67c57e795162529e534da48731f5
Gibran Hemani <explodecomputer@gmail.com>
2020-01-13 21:51:40 +0000

    updated sims

--- null_replication/scripts/aggregate.r
+++ null_replication/scripts/aggregate.r
@@ -1,11 +1,12 @@
 library(tidyverse)
+library(parallel)
 
 fn <- list.files("../data/scratch", pattern=".rdata$", recursive=T)
 
-res <- lapply(fn, function(x) {
-	load(x)
+res <- mclapply(fn, function(x) {
+	load(file.path("../data/scratch", x))
 	as_tibble(res)
-	}) %>% bind_rows()
+	}, mc.cores=10) %>% bind_rows()
 
 save(res, file="../data/aggregate.rdata")
 
