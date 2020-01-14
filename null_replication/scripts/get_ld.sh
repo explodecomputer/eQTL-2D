d04ab8a9fdcd0965821ae8731661c157b40d3358
Gibran Hemani <explodecomputer@gmail.com>
2020-01-14 09:49:23 +0000

    added snplist

--- null_replication/scripts/get_ld.sh
+++ null_replication/scripts/get_ld.sh
@@ -6,5 +6,10 @@ cat <(cut -d " " -f 2 ../data/finemap.txt) <(cut -d " " -f 4 ../data/finemap.txt
 wc -l ../data/snplist.txt
 
 # Get LD between all SNPs
-plink --bfile ../data/combined --extract ../data/snplist.txt --r2 square gz --out ../data/snplist
+plink --bfile ../data/combined --extract ../data/snplist.txt --make-bed --out ../data/snplist
+plink --bfile ../data/snplist --r2 square gz --out ../data/snplist
+awk '{ print $2 }' ../data/snplist.bim > ../data/snplist.snps
+wc -l ../data/snplist.snps
+head ../data/snplist.snps
+head ../data/snplist.txt
 
